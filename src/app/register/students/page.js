9f972f7feeab23a4934966569f2d7d3b1e791491
Peculiar Richard <pecullozie@gmail.com>
2023-08-25 11:41:06 +0100

    Feat: add Register UI for students

--- src/app/register/students/page.js
+++ src/app/register/students/page.js
@@ -8,15 +8,17 @@ import { FcGoogle } from "react-icons/fc";
 import { FaFacebook } from "react-icons/fa";
 import { BsTwitter } from "react-icons/bs";
 
-const StudentLogin = () => {
+const StudentRegister = () => {
   const validationSchema = Yup.object().shape({
     email: Yup.string().email("Invalid email").required("Email is required"),
     password: Yup.string()
       .min(8, "Password must be at least 8 characters")
       .required("Password is required"),
+    name: Yup.string().required("Name is required"),
   });
 
   const initialValues = {
+    name: "",
     email: "",
     password: "",
   };
@@ -31,16 +33,30 @@ const StudentLogin = () => {
       <section className="bg-fill flex items-center">
         <div className="w-[50%] m-auto bg-white p-12 border my-12 flex flex-col items-center">
           <h2 className="text-[#212121] text-[2rem] font-bold">
-            Login to SureCrib
+            Register on SureCrib
           </h2>
           <p className="text-[#757575]">
-            Quick and simple way to find and rent accomodation
+            Quick and easy way to find and rent accomodation
           </p>
           <Formik
             initialValues={initialValues}
             validationSchema={validationSchema}
             onSubmit={handleSubmit}>
             <Form className="flex flex-col w-full px-4 text-[#757575]">
+              <div className="flex flex-col border p-4 my-4">
+                <label htmlFor="password">Full name:</label>
+                <Field
+                  type="text"
+                  id="name"
+                  name="name"
+                  placeholder="John Doe"
+                />
+              </div>
+              <ErrorMessage
+                name="name"
+                component="div"
+                style={{ color: "red", fontSize: "10px" }}
+              />
               <div className="flex flex-col border p-4 my-4">
                 <label htmlFor="email">Email Address:</label>
                 <Field
@@ -77,20 +93,19 @@ const StudentLogin = () => {
                     name="rememberMe"
                     style={{ marginRight: "8px" }}
                   />
-                  Remember Me
+                  I agree to the Terms of Service and Privacy Policy
                 </label>
-                <Link href="#">Forgot Password?</Link>
               </div>
               <button
                 type="submit"
                 className="py-4 px-8 w-full self-center bg-[#1A6177] text-white font-bold my-6 uppercase">
-                Proceed
+                Create an account
               </button>
             </Form>
           </Formik>
 
           <p className="text-[#757575] uppercase">Or use</p>
-          <div className="flex items-center gap-x-8 mt-6">
+          <div className="flex items-center gap-x-8 mt-6 mb-4">
             <button className="bg-gray-100 p-2 rounded-xl">
               <FcGoogle size={35} />
             </button>
@@ -101,10 +116,17 @@ const StudentLogin = () => {
               <FaFacebook size={35} />
             </button>
           </div>
+
+          <p>Already have an account?</p>
+          <Link
+            href="/login/students"
+            className="text-[#1A6177] font-bold mt-2">
+            Log in
+          </Link>
         </div>
       </section>
     </>
   );
 };
 
-export default StudentLogin;
+export default StudentRegister;
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -10,12 +10,28 @@ const Nav = () => {
         <div className="w-[90%] mx-auto flex justify-between items-center sticky z-50 font-extrabold text-black h-[100px]">
           <Image src={logo} alt="SureCrib Logo" width={100} height={100} />
           <div className="w-4/5 flex justify-between justify-self-end gap-10 uppercase">
-            <Link href="/" className="p-3 hover:bg-[#80d0e9] hover:rounded-xl">Home</Link>
-            <Link href="#" className="p-3 hover:bg-[#80d0e9] hover:rounded-xl">Services</Link>
-            <Link href="#" className="p-3 hover:bg-[#80d0e9] hover:rounded-xl">Blog</Link>
-            <Link href="#" className="p-3 hover:bg-[#80d0e9] hover:rounded-xl">About Us</Link>
-            <Link href="#" className="p-3 hover:bg-[#80d0e9] hover:rounded-xl">Register</Link>
-            <Link href="#" className="p-3 bg-[#1A6177] rounded-bl-[25px] rounded-tr-[25px] text-white">Log In</Link>
+            <Link href="/" className="p-3 hover:bg-[#80d0e9] hover:rounded-xl">
+              Home
+            </Link>
+            <Link href="#" className="p-3 hover:bg-[#80d0e9] hover:rounded-xl">
+              Services
+            </Link>
+            <Link href="#" className="p-3 hover:bg-[#80d0e9] hover:rounded-xl">
+              Blog
+            </Link>
+            <Link href="#" className="p-3 hover:bg-[#80d0e9] hover:rounded-xl">
+              About Us
+            </Link>
+            <Link
+              href="/register/students"
+              className="p-3 hover:bg-[#80d0e9] hover:rounded-xl">
+              Register
+            </Link>
+            <Link
+              href="/login/students"
+              className="p-3 bg-[#1A6177] rounded-bl-[25px] rounded-tr-[25px] text-white">
+              Log In
+            </Link>
           </div>
         </div>
       </section>
